4726449c5adc19d8be67da1dc29c5a908521f937
Katya Pavlenko <cakeinpanic@gmail.com>
2020-07-13 13:53:04 +0300

    кккк

--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,5 @@
-import { getInputs } from './action-inputs';
+import * as core from '@actions/core';
 
-async function run(): Promise<void> {
-  getInputs();
-
-  //  const githubConnector = new GithubConnector();
-  //  const jiraConnector = new JiraConnector();
-  //
-  //  if (!githubConnector.isPRAction) {
-  //    console.log('This action meant to be run only on PRs');
-  //    process.exit(0);
-  //  }
-  //
-  //  if (shouldSkipBranch(githubConnector.headBranch, BRANCH_IGNORE_PATTERN)) {
-  //    process.exit(0);
-  //  }
-  //
-  //  const issueKey = githubConnector.getIssueKeyFromTitle();
-  //
-  //  if (!issueKey) {
-  //    process.exit(0);
-  //  }
-  //
-  //  console.log(`JIRA key -> ${issueKey}`);
-  //
-  //  const details = await jiraConnector.getTicketDetails(issueKey);
-  //  await githubConnector.updatePrDetails(details);
-  //} catch (error) {
-  //  console.log({ error });
-  //  core.setFailed(error.message);
-  //  process.exit(1);
-  //}
-}
-
-run();
+const LOL: string = core.getInput('extra');
+console.log(123);
+console.log(LOL + 404);
